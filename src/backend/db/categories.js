b5f29d2219e26a5a10d26487b14d06ffe9a7e035
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-07-22 00:45:05 +0530

    refactor : changes to css

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -11,22 +11,28 @@ export const categories = [
     categoryName: "Smartphones",
     description:
       "Mobile devices that provide advanced computing and communication capabilities.",
+    image:"https://i.postimg.cc/sDndSmPv/smartphones.png"
   },
   {
     _id: uuid(),
     categoryName: "Laptops",
     description: "Portable computing devices designed for mobile use.",
+    image:"https://i.postimg.cc/Y2zw2MhN/laptops.png"
   },
   {
     _id: uuid(),
     categoryName: "Tablets",
     description: "Compact, handheld computing devices with touchscreens.",
+    image:"https://i.postimg.cc/KjfX0rdp/tablets.jpg"
   },
   {
     _id: uuid(),
     categoryName: "Accessories",
     description: "Various gadgets and add-ons to complement tech devices.",
+    image:"https://i.postimg.cc/XYzWMLXT/accessories.jpg"
   },
   
 ];
+
+
 
