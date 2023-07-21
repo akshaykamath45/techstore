ee4a02caf6683dd5f57cb25c215b69361d061b13
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-07-22 00:44:51 +0530

    feat : designing landing page and re

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,27 +1,57 @@
-import React from 'react';
-import { categories } from '../../backend/db/categories.js';
-import { Link } from 'react-router-dom';
-import { useCategoryContext } from '../../contexts/CategoryContext';
+import React from "react";
+import { categories } from "../../backend/db/categories.js";
+import { Link, useNavigate } from "react-router-dom";
+import { useCategoryContext } from "../../contexts/CategoryContext";
+import "./Home.css";
 
 const Home = () => {
   const { setSelectedCategory } = useCategoryContext();
 
+  const navigate = useNavigate();
   const handleCategoryClick = (categoryName) => {
     setSelectedCategory(categoryName);
+    navigate("/products");
+  };
+  const exploreProducts = () => {
+    navigate("/products");
   };
 
   return (
     <div>
-      <h1>This is the Home Page</h1>
-      {categories.map((category) => (
-     
-        <Link key={category._id} to="/products" onClick={() => handleCategoryClick(category.categoryName)}>
-          <div>
-            <h2>{category.categoryName}</h2>
-       
-          </div>
-        </Link>
-      ))}
+      <div className="hero-section">
+        <h1 className="landing-title">Welcome to Tech Store</h1>
+        <h2 className="caption">
+          One stop solution for all your tech needs !!!
+        </h2>
+        <img
+          src="https://i.postimg.cc/V6z0tKgJ/lannding-page-img.jpg"
+          alt="landing-page-img"
+          className="landing-img"
+        />
+        <button onClick={exploreProducts} className="explore-products">
+          Explore Products
+        </button>
+      </div>
+
+      <div className="category-section">
+        <h1>Search by Categories</h1>
+        <div className="category-cards">
+          {categories.map((category) => (
+            <div
+              className="category-card"
+              key={category._id}
+              onClick={() => handleCategoryClick(category.categoryName)}
+            >
+              <h2 className="category-name">{category.categoryName}</h2>
+              <img
+                src={category.image}
+                alt="category-image"
+                className="category-image"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
