42ae538d9adce2b1b4b0e914a088040239e32198
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-07-22 17:52:43 +0530

    fix : handling cart empty edge case

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,40 +1,67 @@
 import React, { useContext } from "react";
 import { CartContext } from "../../contexts/CartContext";
 import "./Cart.css";
-
+import emptyCart from "../../assets/emptyCart.svg";
 const Cart = () => {
-  const { cart, handleDeleteFromCart, handleIncreaseQuantity, handleDecreaseQuantity } = useContext(CartContext);
+  const {
+    cart,
+    handleDeleteFromCart,
+    handleIncreaseQuantity,
+    handleDecreaseQuantity,
+  } = useContext(CartContext);
 
   return (
     <div className="cart-container">
-      <div className="cart-items">
-        <h2>Cart Items</h2>
-        {cart.map((item) => (
-          <div key={item._id} className="cart-item">
-            <img
-              src={item.image}
-              alt={item.name}
-              className="cart-item-image"
-            />
-            <div className="cart-item-details">
-              <p>{item.name}</p>
-              <p>Price: {item.price}</p>
-              <div className="quantity-container">
-                <button onClick={() => handleDecreaseQuantity(item._id)} className='cart-quantity-btn'>-</button>
-                <p className='item-quantity'>{item.quantity}</p>
-                <button onClick={() => handleIncreaseQuantity(item._id)} className='cart-quantity-btn'>+</button>
+      {cart.length === 0 ? (
+        <div className="empty-cart">
+          <h1>Your cart is empty</h1>
+          <img src={emptyCart} alt="empty-cart" className="empty-cart-svg" />
+        </div>
+      ) : (
+        <div className="cart-items">
+          <h2>Cart Items</h2>
+          {cart.map((item) => (
+            <div key={item._id} className="cart-item">
+              <img
+                src={item.image}
+                alt={item.name}
+                className="cart-item-image"
+              />
+              <div className="cart-item-details">
+                <p>{item.name}</p>
+                <p>Price: {item.price}</p>
+                <div className="quantity-container">
+                  <button
+                    onClick={() => handleDecreaseQuantity(item._id)}
+                    className="cart-quantity-btn"
+                  >
+                    -
+                  </button>
+                  <p className="item-quantity">{item.quantity}</p>
+                  <button
+                    onClick={() => handleIncreaseQuantity(item._id)}
+                    className="cart-quantity-btn"
+                  >
+                    +
+                  </button>
+                </div>
+                <button
+                  onClick={() => handleDeleteFromCart(item._id)}
+                  className="delete-cart-btn"
+                >
+                  Delete from Cart
+                </button>
               </div>
-              <button onClick={() => handleDeleteFromCart(item._id) } className='delete-cart-btn'>
-                Delete from Cart
-              </button>
             </div>
+          ))}
+          <div className="total-amount">
+            <h2>Total Amount</h2>
+            <p>
+              ${cart.reduce((acc, item) => acc + item.price * item.quantity, 0)}
+            </p>
           </div>
-        ))}
-      </div>
-      <div className="total-amount">
-        <h2>Total Amount</h2>
-        <p>${cart.reduce((acc, item) => acc + item.price * item.quantity, 0)}</p>
-      </div>
+        </div>
+      )}
     </div>
   );
 };
