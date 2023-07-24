5d10efe7a8f40ae993223025921ac61f9f67ef8c
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-07-25 01:14:30 +0530

    style : cart and wishlist count on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useState,useContext } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import {CartContext} from "../contexts/CartContext";
+import {WishlistContext} from "../contexts/WishlistContext";
 import "./Navbar.css";
 const Navbar = ({ techProducts }) => {
   const [searchValue, setSearchValue] = useState("");
   const [showModal, setShowModal] = useState(false);
   const [filteredProducts, setFilteredProducts] = useState([]);
-
+  const {cart}=useContext(CartContext)
+  const {wishlist}=useContext(WishlistContext)
   const navigate = useNavigate();
   const handleSearch = (e) => {
     const value = e.target.value;
@@ -83,9 +86,12 @@ const Navbar = ({ techProducts }) => {
           viewBox="0 0 16 16"
         >
           <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-        </svg>
+        </svg>{
+          cart.length>0 && <span className="count-badge cart-count">{cart.length}</span>
+        }
+       
       </NavLink>
-      <NavLink to="/wishlist" className="nav-bar-item">
+      <NavLink to="/wishlist" className="nav-bar-item wishlist">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
@@ -99,6 +105,10 @@ const Navbar = ({ techProducts }) => {
             d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
           />
         </svg>
+            {
+              wishlist.length>0 && <span className="count-badge wishlist-count">{wishlist.length}</span>
+            }
+ 
       </NavLink>
     </nav>
   );
