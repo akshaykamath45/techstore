21c504d932e9e44097bc6c0522fc286a9e5f8e8a
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-07-25 01:32:43 +0530

    style : handling edge case for empty wishlist

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,31 +1,42 @@
 import React from "react";
 import { useContext } from "react";
 import { WishlistContext } from "../../contexts/WishlistContext";
+import emptyWishlist from "../../assets/emptyWishlist.svg";
 import "./Wishlist.css";
+
 const Wishlist = () => {
   const { wishlist } = useContext(WishlistContext);
   const { handleDeleteFromWishlist } = useContext(WishlistContext);
+
   return (
-    <div className="wishlist">
-    
-      <h2 className='wishlist-items'>Items in  Wishlist : {wishlist.length}</h2>
-      <div className="wishlist-page">
-      {wishlist.map((item) => {
-        return (
-          <div className='wishlist-card'>
-            <img src={item.image} alt="wishlisted" className='wishlist-image' />
-            <h2>{item.name}</h2>
-            <p>{item.price}</p>
-           
-            <button onClick={() => handleDeleteFromWishlist(item._id)} className="delete-wishlist-btn">
-              Delete from Wishlist
-            </button>
-         
+    <div className="wishlist-container">
+      {wishlist.length === 0 ? (
+        <div className="empty-wishlist">
+          <h1>Nothing to show here</h1>
+          <img src={emptyWishlist} alt="wishlist-empty" className="empty-wishlist-image" />
+        </div>
+      ) : (
+        <div>
+          <h2 className="wishlist-items">Items in Wishlist: {wishlist.length}</h2>
+          <div className="wishlist-page">
+            {wishlist.map((item) => {
+              return (
+                <div className="wishlist-card" key={item._id}>
+                  <img src={item.image} alt="wishlisted" className="wishlist-image" />
+                  <h2>{item.name}</h2>
+                  <p>{item.price}</p>
+                  <button
+                    onClick={() => handleDeleteFromWishlist(item._id)}
+                    className="delete-wishlist-btn"
+                  >
+                    Delete from Wishlist
+                  </button>
+                </div>
+              );
+            })}
           </div>
-        );
-      })}
-      </div>
-  
+        </div>
+      )}
     </div>
   );
 };
